43bf5cb2252489699bf9d8175c7ded06731e4795
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-06-14 22:17:30 +0100

    first commit of 14th june

--- app/src/main/java/com/example/android/execises/MainActivity.kt
+++ app/src/main/java/com/example/android/execises/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
